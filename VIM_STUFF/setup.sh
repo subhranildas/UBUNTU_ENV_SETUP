e90d5de62a37ca82ba866c77f95c63e2eef97578
Subhranil Das <subhranil.believe@gmail.com>
2024-06-02 19:29:17 +0530

    Add script to update bashrc
    Signed-off-by: Subhranil Das <subhranil.believe@gmail.com>

---
+++ VIM_STUFF/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+USER_HOME=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)
+OLD_VIMRC="${USER_HOME}/.vimrc"
+
+# Check if the .vimrc file exists or not
+if [ -e ${OLD_VIMRC} ]
+then 
+    # .vimrc exists
+    # rename .vimrc to .vimrc.backup
+    if ! mv ${OLD_VIMRC} ${USER_HOME}/.vimrc.bak; then
+        echo "Can't move old vimrc file !!"
+        exit 1
+    fi
+fi
+
+# Copy the .vimrc from here to home folder
+cp $PWD/.vimrc ${USER_HOME}/
