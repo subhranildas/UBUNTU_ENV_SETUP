e79b078f736e657db9ff69d83340c2751f4de8fb
Subhranil Das <subhranil.believe@gmail.com>
2024-06-26 20:29:46 +0530

    Update bashrc setup Script
    - setup Script compares old and new bashrc
      before backup and replacement
    - Update README.md file for bashrc setup
    - Add LS color modification guide in README.md
    - Change LS color for directory
    
    Signed-off-by: Subhranil Das <subhranil.believe@gmail.com>

--- BASH_CONFIG/setup.sh
+++ BASH_CONFIG/setup.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/bash
+#! /bin/bash
 
 USER_HOME=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)
 OLD_BASHRC="${USER_HOME}/.bashrc"
+NEW_BASHRC="$PWD/.bashrc"
 
 command_exists() {
     command -v $1 >/dev/null 2>&1
@@ -9,24 +10,31 @@ command_exists() {
 
 # Check if batcat is installed or not
 if command_exists batcat; then
-    echo "BATCAT Already installed !! Good Job !!"
+    echo "<-> BATCAT Already installed !!"
 else
-    echo "Installing BATCAT"
+    echo "<-> Installing BATCAT"
     if ! sudo apt install bat; then
-        echo "Something went Wrong During BATCAT installation !"
+        echo "<-> Something went Wrong During BATCAT installation !!"
         exit 1
     else
-        echo "BATCAT installed successfully !!"
+        echo "<-> BATCAT installed successfully !!"
     fi
 fi
 
-echo "Replacing BASHRC file ..."
 
-if ! mv ${OLD_BASHRC} ${USER_HOME}/.bashrc.bak; then
-    echo "Can't move old bashrc file !!"
-    exit 1
+if cmp -s "$NEW_BASHRC" "$OLD_BASHRC"; then
+    echo "<-> Same bashrc !! Replacement not Required !!"
 else
-    cp $PWD/.bashrc ${USER_HOME}/
-    source ~/.bashrc
+    if ! mv ${OLD_BASHRC} ${USER_HOME}/.bashrc.bak; then
+        echo "<-> Can't move old bashrc file !!"
+        exit 1
+    else
+        echo "<-> Replacing BASHRC file ..."
+        cp $NEW_BASHRC ${USER_HOME}/
+        echo "<-> .bashrc file Replaced !!"
+    fi
 fi
+
+source ~/.bashrc
+echo "<-> Done !!!"
 
