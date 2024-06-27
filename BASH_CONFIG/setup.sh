ef72fdd4f4f36df8a57f87e949d11978834378a2
Subhranil Das <subhranil.believe@gmail.com>
2024-06-27 20:53:38 +0530

    Update bashrc setup Script
    - Give user option to exec bash if
      bashrc replacement happened
    - Change ls dir color to light blue(94)
    
    Signed-off-by: Subhranil Das <subhranil.believe@gmail.com>

--- BASH_CONFIG/setup.sh
+++ BASH_CONFIG/setup.sh
@@ -1,8 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
 USER_HOME=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)
 OLD_BASHRC="${USER_HOME}/.bashrc"
 NEW_BASHRC="$PWD/.bashrc"
+BASHRC_REPLACEMENT_FLAG=0
 
 command_exists() {
     command -v $1 >/dev/null 2>&1
@@ -32,9 +33,22 @@ else
         echo "<-> Replacing BASHRC file ..."
         cp $NEW_BASHRC ${USER_HOME}/
         echo "<-> .bashrc file Replaced !!"
+        BASHRC_REPLACEMENT_FLAG=1
     fi
 fi
 
-source ~/.bashrc
 echo "<-> Done !!!"
 
+# Execute bash to apply changes
+if [ $BASHRC_REPLACEMENT_FLAG == 1 ]
+then
+    read -p "<-> Execute bash to apply changes ? [Y/N]" yn
+
+    case $yn in
+        [yY] ) exec bash;;
+        [nN] ) echo "<-> Source bashrc to apply changes !!";;
+        * ) echo "<-> Da Fak !!";;
+    esac
+fi
+
+echo "<-> Run install_bashrc_support to install necessary support files if not already installed  !!"
