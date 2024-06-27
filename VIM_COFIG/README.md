fbe779f0ca2dafe27c4a1509a2c55fc2f8fab5cc
Subhranil Das <subhranil.believe@gmail.com>
2024-06-27 20:08:28 +0530

    Update vimrc setup Script
    - Setup checks for existance of vimrc
    - Setup compares old vimrc with new vimrc
      before backup and replacement
    
    Signed-off-by: Subhranil Das <subhranil.believe@gmail.com>

--- VIM_COFIG/README.md
+++ VIM_COFIG/README.md
@@ -1,5 +1,5 @@
 # INSTRUCTIONS #
 
-- Add the .bashrc file in the home/user/ folder for VIM to use.
+- Add the .bashrc file in the home/<user>/ folder for VIM to use.
 - Use https://ss64.com/bash/syntax-colors.html for all Xterm Color References.
-    
+
--- VIM_COFIG/setup.sh
+++ VIM_COFIG/setup.sh
@@ -1,19 +1,34 @@
-#! /usr/bin/bash
+#!/bin/bash
 
 USER_HOME=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)
 OLD_VIMRC="${USER_HOME}/.vimrc"
+NEW_VIMRC="$PWD/.vimrc"
 
 # Check if the .vimrc file exists or not
 if [ -e ${OLD_VIMRC} ]
 then
+    echo "<-> Existing vimrc found !!"
     # .vimrc exists
-    # rename .vimrc to .vimrc.backup
-    if ! mv ${OLD_VIMRC} ${USER_HOME}/.vimrc.bak; then
-        echo "Can't move old vimrc file !!"
-        exit 1
+    if cmp -s "$NEW_VIMRC" "$OLD_VIMRC"; then
+        echo "<-> Same vimrc !! Replacement not Required !!"
     else
-        # Copy the .vimrc from here to home folder
-        cp $PWD/.vimrc ${USER_HOME}/
+        # Rename old .vimrc to .vimrc.bak
+        echo "<-> Backing up old vimrc !!"
+        if ! mv ${OLD_VIMRC} ${USER_HOME}/.vimrc.bak; then
+            echo "<-> Can't move old vimrc file !!"
+            exit 1
+        else
+            echo "<-> Adding new vimrc to user HOME !!"
+            # Copy the .vimrc from here to user home folder
+            cp ${NEW_VIMRC} ${USER_HOME}/
+        fi
     fi
+else
+    echo "<-> No existing vimrc !!"
+    echo "<-> Adding vimrc file to user HOME !!"
+    # Copy the .vimrc from here to user home folder
+    cp ${NEW_VIMRC} ${USER_HOME}/
 fi
+
+echo "Done !!"
 
