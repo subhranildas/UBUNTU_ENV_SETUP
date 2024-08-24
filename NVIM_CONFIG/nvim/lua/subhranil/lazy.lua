1e3d699a66c00b13b391daaaf9aadb17bfbb1b66
Subhranil Das <subhranil.believe@gmail.com>
2024-08-24 18:13:16 +0530

    Add plugin to NVIM Config
    - Add Comment plugin for better comment
    - Add surround for text surround functionality
    - Update bufferline seperator to thin
    
    Signed-off-by: Subhranil Das <subhranil.believe@gmail.com>

--- NVIM_CONFIG/nvim/lua/subhranil/lazy.lua
+++ NVIM_CONFIG/nvim/lua/subhranil/lazy.lua
@@ -11,5 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("subhranil.plugins")
+require("lazy").setup("subhranil.plugins", {
+    change_detection = {
+        notify = false,
+    },
+})
 
